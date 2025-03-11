33bb131382cf6231c80c555e0e3519906acbb3bc
vuttgl123 <phamtuanvu1401@gmail.com>
2025-03-11 23:43:46 +0700

    Update URL

--- src/components/api/authApi.js
+++ src/components/api/authApi.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = "http://localhost:5120/api/users";
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5120/api/users";
 
 export const login = async (email, password) => {
     const response = await fetch(`${API_BASE_URL}/login`, {
